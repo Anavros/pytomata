7464e2be787bff2ef9242c6753a0830273f4146c
John Dobbs <jtdobbs1@me.com>
2016-09-13 18:32:00 -0400

    Basic texture rendering with random values.

--- main.py
+++ main.py
@@ -6,20 +6,27 @@ import rocket
 from rocket.aux import load_shaders
 
 program = load_shaders('vertex.glsl', 'fragment.glsl')
+texture = np.random.randint(0, 255, size=(512, 512), dtype=np.uint8)
 vertices = np.array([
     (-1, +1), (+1, +1),
     (-1, -1), (+1, -1),
 ], dtype=np.float32)
+texcoords = np.array([
+    (0, 1), (1, 1),
+    (0, 0), (1, 0),
+], dtype=np.float32)
 
 
 def main():
-    rocket.prep()
+    rocket.prep(size=(512, 512))
     rocket.launch()
 
 
 @rocket.attach
 def draw():
-    program['vert'] = gloo.VertexBuffer(vertices)
+    program['tex'] = texture
+    program['vertex'] = gloo.VertexBuffer(vertices)
+    program['texcoord'] = gloo.VertexBuffer(texcoords)
     program.draw('triangle_strip')
 
 
--- vertex.glsl
+++ vertex.glsl
@@ -1,7 +1,9 @@
 #version 120
 
-attribute vec2 vert;
+attribute vec2 vertex;
+attribute vec2 texcoord;
 
 void main(void) {
-    gl_Position = vec4(vert, 0.0, 1.0);
+    gl_Position = vec4(vertex, 0.0, 1.0);
+    gl_TexCoord[0] = vec4(texcoord, 0.0, 0.0);
 }
